5f39aa24e7f7099e6de11b8038f9c3a2c814f8ae
ThigeJay <thigejames25@gmail.com>
2024-04-24 19:48:39 +0300

    2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+function handleResponseFromAPI(promise) {
+    promise
+        .then((response) => {
+            console.log('Got a response from the API');
+            return { status: 200, body: 'success' };
+        })
+        .catch(() => new Error());
+}
+
+export default handleResponseFromAPI;
